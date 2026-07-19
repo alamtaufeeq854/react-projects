0687d6874dab006aed6d3ea17fae057cc1dd4ce7
Taufeeq Alam <alamtaufeeq854@gmail.com>
2026-07-19 10:02:24 +0530

    add back button

--- 05.tic_tac_toe_game/src/App.js
+++ 05.tic_tac_toe_game/src/App.js
@@ -2,7 +2,19 @@ import "./App.css";
 import TicTacToe from "./components/TicTacToe/TicTacToe";
 
 function App() {
-  return <TicTacToe />;
+  return (
+    <>
+      <button
+        onClick={() => {
+          window.location.href =
+            "https://alamtaufeeq854.github.io/react-projects/";
+        }}
+        className="home-btn">
+        Back
+      </button>{" "}
+      <TicTacToe />
+    </>
+  );
 }
 
 export default App;
